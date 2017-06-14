cf9464d2508a52992d330f569cc5793447613d8e
Gaston Dario Napoli <Gaston Dario Napoli>
2017-06-13 23:39:45 -0300

    Error en entidad Brand

---
+++ backoffice-api/src/main/java/com/vates/wifibus/api/model/AppConfiguration.java
@@ -0,0 +1,14 @@
+package com.vates.wifibus.api.model;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+public class AppConfiguration {
+
+    private String cobrand;
+    
+    private String logoImage;
+    
+    private String backgroundImage;
+    
+}
