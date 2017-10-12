c693963ecae19e40d3d48825cb965a520182605b
lustubb <luis.x.stubbia.-nd@disney.com>
2017-10-12 13:23:45 -0300

    AuditConfig - Faltaba agregar este bean para que setee las demas propiedades

---
+++ backoffice-api/src/main/java/com/vates/wifibus/backoffice/api/config/AuditConfig.java
@@ -0,0 +1,35 @@
+package com.vates.wifibus.backoffice.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * 
+ * @author Gaston Napoli
+ *
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+
+	@Bean
+	public AuditorAware<String> createAuditorProvider() {
+		return new SecurityAuditor();
+	}
+
+	@Bean
+	public AuditingEntityListener createAuditingListener() {
+		return new AuditingEntityListener();
+	}
+
+	public static class SecurityAuditor implements AuditorAware<String> {
+
+		@Override
+		public String getCurrentAuditor() {
+			return "<BACKEND>";
+		}
+	}
+}
